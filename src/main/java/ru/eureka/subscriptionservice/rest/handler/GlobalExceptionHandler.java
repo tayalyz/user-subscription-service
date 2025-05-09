59e486dc66266d962ecf06bf25d1b27b1b0fde89
tayal <tayalis.6107038@gmail.com>
2025-05-09 05:58:30 +0300

    HttpStatus fix

--- src/main/java/ru/eureka/subscriptionservice/rest/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/eureka/subscriptionservice/rest/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.eureka.subscriptionservice.rest.handler;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,17 +19,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     ResponseEntity<String> handle(DataIntegrityViolationException e) {
-        return ResponseEntity.status(409).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     ResponseEntity<String> handle(HttpMessageNotReadableException e) {
-        return ResponseEntity.status(400).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     ResponseEntity<String> handle(EntityNotFoundException e) {
-        return ResponseEntity.status(404).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     private ResponseEntity<String> handleResponseStatusException(ResponseStatusException e) {
